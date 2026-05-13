ebeff2bc0f1b579a39d6879af81076cf57e447ec
Ayoub Zaki <ayoub.zaki@embetrix.com>
2026-05-13 20:07:27 +0200

    init-crypto.sh: use insecure master and user key untill rpifwcrypto trusted key kernel patch is rebased

--- recipes-core/initrdscripts/files/init-crypto.sh
+++ recipes-core/initrdscripts/files/init-crypto.sh
@@ -12,9 +12,12 @@
 setup_encrypted_keys() {
 
 	if rpi-fw-crypto pubkey --key-id "$OTP_KEY_ID" --out /dev/null 2>/dev/null; then
-		klog "OTP key provisioned, using trusted master key"
-		MASTER_KEY_TYPE="trusted"
-		ENC_KEY_MASTER="trusted:kmk"
+#		klog "OTP key provisioned, using trusted master key"
+		klog "OTP key provisioned but still sing user master key (not secure) untill rpi-fw-crypto patch is in place"
+#		MASTER_KEY_TYPE="trusted"
+#		ENC_KEY_MASTER="trusted:kmk"
+		MASTER_KEY_TYPE="user"
+		ENC_KEY_MASTER="user:kmk"
 	else
 		klog "WARNING: OTP key not provisioned, using user master key (not secure)"
 		MASTER_KEY_TYPE="user"
